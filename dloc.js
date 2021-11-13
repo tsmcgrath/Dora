3fe77931d540e1cde4caf2aac8a101a9d8340ba7
Tim McGrath <tim@wanderfeast.com>
2021-11-13 10:51:55 -0800

    Updates and debugging
    Updates and debugging

--- dloc.js
+++ dloc.js
@@ -1,81 +1,86 @@
-var dloc = { "type": "FeatureCollection",
+var dloc = { "type": "featureCollection",
     "features": [
-        { "type": "Feature",
+        { "type": "feature",
             "geometry": {"type": "Point", "coordinates": [112.9644, 22.7654]},
             "properties": {"name": "He-Shan",
                 "color": "red",
                 "story": "Great-great Grandma Dora's mom is born in He-Shan city (鹤山市), Guangdong Province (廣東省), China on April 14, 1903. So, that was Amelia's Great-great-great Grandmother."
             }
         },
-        { "type": "Feature",
+        { "type": "feature",
         "geometry": {"type": "Point", "coordinates": [-122.4078, 37.7941]},
         "properties": {"name": "Chinatown",
             "color": "red",
             "story": "Ah Fong Wood Chun (馮活泉) was Grandpa Fong to your great grandmother. He made a fortune in San Francisco."
         }
     },
-    { "type": "Feature",
+    { "type": "feature",
         "geometry": {"type": "Point", "coordinates": [114.1694, 22.3193]},
         "properties": {"name": "Hong Kong",
             "color": "red",
             "story": "He returned to Hong Kong after he lost his fortune and adopted Fong Lai Kam(馮麗琴), Grandma Saw's mother."
         }
     },
-    { "type": "Feature",
+    { "type": "feature",
         "geometry": {"type": "Point", "coordinates": [113.5439, 22.1987]},
         "properties": {"name": "Macau",
             "color": "red",
             "story": "Great-great Grandpa Fong took Lai Kam and his wife to Macau before World War II."
         }
     },
-    { "type": "Feature",
+    { "type": "feature",
         "geometry": {"type": "Point", "coordinates": [113.2644, 23.1291]},
         "properties": {"name": "Canton (Guangzhou)",
             "color": "red",
             "story": "Great-great Grandpa Fong took Lai Kam and his wife and fled to Canton at the start of World War II. Because the Japanese were invading China!"
         }
     },
-    { "type": "Feature",
+    { "type": "feature",
         "geometry": {"type": "Point", "coordinates": [111.56896901373813, 22.771892080575782]},
         "properties": {"name": "Luo-Ding City (羅定市)",
             "color": "red",
             "story": "Great Grandma Saw Moved to Luo-Ding City (羅定市) in the winter of 1941 during World War 2. When they lived there her mom saved her 3 year old sister Pam from drowning in the river."
         }
     },
-    { "type": "Feature",
+    { "type": "feature",
         "geometry": {"type": "Point", "coordinates": [111.7859, 23.1437]},
         "properties": {"name": "Deqing (德慶)",
             "color": "red",
             "story": "In 1944 they moved to Deqing (德慶) after staying in a Haunted House in Canton."
         }
     },
-    { "type": "Feature",
+    { "type": "feature",
         "geometry": {"type": "Point", "coordinates": [111.2791, 23.4770]},
         "properties": {"name": "Wuzhou",
+        "color": "red",
         "story": "Great-great Grandpa Fong worked in Wuzhou and hid aboard a Japanese boat to get back to the family in Deqing"
         }
     },
-    { "type": "Feature",
+    { "type": "feature",
         "geometry": {"type": "Point", "coordinates": [108.89213818865544, 23.51899033827736]},
         "properties": {"name": "Ma-xu (馬墟)",
+        "color": "red",
         "story": "From Deqing they moved to Ma-xu (馬墟) where Great-great Grandpa Fong helped people with the best medicine he knew. The people were grateful and gave them food to eat because no one had any money."
         }
     },
-    { "type": "Feature",
+    { "type": "feature",
         "geometry": {"type": "Point", "coordinates": [113.2644, 23.1291]},
         "properties": {"name": "Canton (Guangzhou)",
+        "color": "red",
         "story": "Once World War II ended, Great-great Grandpa Fong took his family back to Canton."
         }
     },
-    { "type": "Feature",
+    { "type": "feature",
         "geometry": {"type": "Point", "coordinates": [-117.2612, 34.0483]},
         "properties": {"name": "Loma Linda",
+        "color": "red",
         "story": "Great Grandma Saw married Great Grandpa Saw (Eng Chuan(永泉)) and they lived in Loma Linda while Grandma Saw went to nursing school and Grandpa Saw went to medical school."
         }
     },
-    { "type": "Feature",
+    { "type": "feature",
         "geometry": {"type": "Point", "coordinates": [-117.26944646613603, 34.050620815684454]},
         "properties": {"name": "Loma Linda Pool",
+        "color": "red",
         "story": "Great Grandma Saw babysat 10 kids during the day and two babies at night. She put the older kids in swim lessons while she finished her Microbiology class."
         }
     }
